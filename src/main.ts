018a0e5b81e86457cb072973305bb2d511e2ef1c
Thiago Lanza <lanzathm@gmail.com>
2021-11-16 20:05:32 -0300

    Code cleanup

--- src/main.ts
+++ src/main.ts
@@ -8,43 +8,26 @@ const client = mqttConnection('mqtt://teste.gholias.com:1884');
 const requestTopic = 'its/165063/sys/info';
 const responseTopic = 'its_dados';
 
+// Mensagem para recebimento de todos os sensores na resposta.
 const message = JSON.stringify({ n: 'read_all', u: '/', vb: true });
 
-
-// Se inscreve no tópico
 client.on('connect', () => {
-  console.log('Connected to mqtt://teste.gholias.com:1884');
+  console.log('Conectado em mqtt://teste.gholias.com:1884');
 
+  // Se inscreve nos tópicos
   client.subscribe(requestTopic);
   client.subscribe(responseTopic);
 
   client.publish(requestTopic, message, { qos: 0, retain: false });
 
   client.on('message', (topic, payload) => {
+    // Converte a payload (buffer) em string para que se possa ser convertido em objeto depois.
     const response = payload.toString();
-    console.log(`\ntopic: ${topic} \npayload: ${payload}`);
+    console.log(`\nTópico: ${topic} \nMensagem: ${payload}`);
+
+    // Exibe em formato de tabela o objeto mensagem.
     console.table(JSON.parse(response));
-  })
+  });
 });
 
-client.on('message', (topic, payload) => console.log('voltou payload ' + payload.toString() + '\n no tópico ' + topic));
-
-// const data: Response[] = [
-//   { bn: 165063, bt: 1637075109 },
-//   { n: 'battery', u: '%EL', v: 74.7 },
-//   { n: 'rssi', u: 'dBW', v: -125 },
-//   { n: 'ext_pwr', vb: true },
-//   { n: 'update', vb: true },
-//   { n: 'c1_status', vb: false },
-//   { n: 'c2_status', vb: false },
-//   { n: 'relay', vs: 'NC' },
-//   { n: '288fd1ec09000096', u: 'Cel', v: 24.8 },
-//   { n: 'emc_e1_curr', u: 'A', v: 0 },
-//   { n: 'emc_e2_curr', u: 'A', v: 0.0004 },
-//   { n: 'emc_e3_curr', u: 'A', v: 0.0003 },
-//   { n: 'emc_e4_curr', u: 'A', v: 0.0003 },
-// ];
-
-// console.table(data);
-
-// rl.question('Press [Enter] to exit...', () => process.exit());
+rl.question('Press [Enter] anytime to exit...\n\n', () => process.exit());
